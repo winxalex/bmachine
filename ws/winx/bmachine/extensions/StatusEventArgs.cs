60adc9f4607200a75016e93b5251caea67993ee8
xxx <winxalex@yahoo.com>
2014-11-22 20:10:54 +0100

    StatusEventArgs forgot to include in git

---
+++ ws/winx/bmachine/extensions/StatusEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace ws.winx.bmachine.extensions
+{
+		public delegate void StatusUpdateHandler (object sender,StatusEventArgs e);
+
+		public class StatusEventArgs : EventArgs
+		{
+				public BehaviourMachine.Status status { get; private set; }
+		
+				public StatusEventArgs (BehaviourMachine.Status status)
+				{
+						this.status = status;
+				}
+		}
+
+
+}
