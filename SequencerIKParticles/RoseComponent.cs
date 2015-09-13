8c81a3aee96e75f53df6360a6b2334102e2ba909
xxx <winxalex@yahoo.com>
2015-09-13 16:17:38 +0200

    ScenesIK Test and Resources

---
+++ SequencerIKParticles/RoseComponent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoseComponent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	public void GrabTheFlower(Transform parent){
+
+		this.transform.parent = parent;
+
+		this.transform.rotation=Quaternion.Euler(new Vector3(90,0, 0));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ SequencerIKParticles/UnityChenFaceLayerWeigthAnimatedValues.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using ws.winx.unity;
+
+public class UnityChenFaceLayerWeigthAnimatedValues : MonoBehaviour,IAnimatedValues {
+
+	public Animator animator;
+
+
+	public float FaceLayerWeight;
+	public int FaceLayerIndex;
+
+	#region IAnimatedValues implementation
+	public void UpdateValues ()
+	{
+		animator.SetLayerWeight (FaceLayerIndex, FaceLayerWeight);
+
+		//Debug.Log ("Layer " + FaceLayerIndex + " weight:" + animator.GetLayerWeight(FaceLayerIndex));
+	}
+	public void ResetValues ()
+	{
+		FaceLayerWeight = 0f;
+		animator.SetLayerWeight (FaceLayerIndex, FaceLayerWeight);
+	}
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
